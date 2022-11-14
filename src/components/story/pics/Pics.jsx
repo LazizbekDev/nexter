64b29a149e0dd275a447a4c85d8a8f357a65a338
LazizbekDev <lazizbektojiboyev0611@gmail.com>
2022-11-13 22:10:10 -0800

    story content completed

--- src/components/story/pics/Pics.jsx
+++ src/components/story/pics/Pics.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import story from "../../../img/story-1.jpeg";
+import story2 from "../../../img/story-2.jpeg";
 import "./Pics.scss";
 
 const Pics = () => {
   return (
-    <div className='pics'>Pics</div>
+    <div className='pics'>
+      <img src={story} className={"pics-img--1"} alt={"story 1"} />
+      <img src={story2} className={"pics-img--2"} alt={"story 2"} />
+    </div>
   )
 }
 
