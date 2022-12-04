07e26a6a829dbec18f3cfdf98a419c0ad10d6762
LazizbekDev <lazizbektojiboyev0611@gmail.com>
2022-12-04 03:08:26 -0500

    play sound when people click to sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import "./Sidebar.scss";
-import { VscMenu, VscChromeClose } from "react-icons/vsc"
+import {VscMenu, VscChromeClose} from "react-icons/vsc"
+import clickSound from "../../sound/click.wav";
 
 const Sidebar = ({onClick, className}) => {
-  return (
-    <div className='sidebar'>
-      <button className={`nav-btn ${className}`} onClick={onClick}>
-          {className !== 'sidebar-btn' ? <VscMenu size={'4rem'} /> : <VscChromeClose size={'4rem'} />}
-      </button>
-    </div>
-  )
+
+    const playOnClick = () => new Audio(clickSound).play()
+    return (
+        <div className='sidebar'>
+            <button className={`nav-btn ${className}`} onClick={onClick}>
+                {className !== 'sidebar-btn' ? (
+                    <VscMenu
+                        size={'4rem'}
+                        onClick={playOnClick}
+                        style={{cursor: "pointer"}}
+                    />
+                ) : (
+                    <VscChromeClose
+                        size={'4rem'}
+                        onClick={playOnClick}
+                        style={{cursor: "pointer"}}
+                    />
+                )}
+            </button>
+        </div>
+    )
 }
 
 export default Sidebar
