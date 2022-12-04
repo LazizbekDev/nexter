9de3c307c1e23f9d961a6cdcaa1ea4a371166f1b
LazizbekDev <lazizbektojiboyev0611@gmail.com>
2022-12-04 03:08:37 -0500

    console.log removed

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-// import { IoIosExpand, IoPricetagOutline } from 'react-icons/io'
 import { data } from './homeData';
 
 import "./_Home.scss";
 import HomeItem from './HomeItem';
 
 const Home = () => {
-  console.log(data)
   return (
     <div className='homes'>
 
