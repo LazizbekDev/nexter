1cca461e7b04f76a5479c71e508747ea12ce15a1
LazizbekDev <lazizbektojiboyev0611@gmail.com>
2022-12-04 03:50:11 -0500

    added new features

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
-import { data } from './homeData';
+import {data} from './homeData';
 
 import "./_Home.scss";
 import HomeItem from './HomeItem';
 
 const Home = () => {
-  return (
-    <div className='homes'>
+    return (
+        <div className='homes'>
 
-      {data.map(house => (
-        <HomeItem
-          img={house.img}
-          name={house.name}
-          location={house.location}
-          area={house.area}
-          room={house.room}
-          price={house.price}
-        />
-      ))}
-    </div>
-  )
+            {data.map(house => (
+                <HomeItem
+                    key={house.img}
+                    img={house.img}
+                    name={house.name}
+                    location={house.location}
+                    area={house.area}
+                    room={house.room}
+                    price={house.price}
+                />
+            ))}
+        </div>
+    )
 }
 
 export default Home
