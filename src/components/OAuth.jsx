982a052564955ee2a229f676a5682b48e5369101
LazizbekDev <lazizbektojiboyev0611@gmail.com>
2022-12-15 13:03:03 -0500

    google oAuth functionality

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,18 +1,42 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { doc, setDoc, getDoc } from "firebase/firestore";
+import {doc, setDoc, getDoc, serverTimestamp} from "firebase/firestore";
 import { toast } from "react-toastify";
 import { FcGoogle } from "react-icons/fc";
+import {db} from "../firebase.config";
 
 const OAuth = () => {
     const navigate = useNavigate();
     const location = useLocation();
 
+    const onAuth = async () => {
+        try {
+            const auth = getAuth();
+            const provider = new GoogleAuthProvider();
+            const result = await signInWithPopup(auth, provider);
+
+            const user = result.user;
+            const docRef = doc(db, 'users', user.uid);
+            const docSnap = await getDoc(docRef)
+
+            if (!docSnap.exists()) {
+                await setDoc(doc(db, 'users', user.uid), {
+                    name: user.displayName,
+                    email: user.email,
+                    timestamp: serverTimestamp()
+                })
+            }
+            navigate('/')
+        } catch (err) {
+            toast.error('Could not authorize with google')
+        }
+    }
+
     return (
         <div>
             OR
             <p>Sign {location.pathname === '/login' ? "in" : "up"} with</p>
-            <button>
+            <button onClick={onAuth}>
                 <FcGoogle />
             </button>
         </div>
