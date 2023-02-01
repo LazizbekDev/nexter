f83bcda43e145e4f8645345b372d5df5ef4be253
lazizbekdev <lazizbektojiboyev0611@gmail.com>
2023-02-01 13:14:43 +0500

    load more function in offer page

--- src/pages/Offer.jsx
+++ src/pages/Offer.jsx
@@ -1,5 +1,5 @@
 import {useEffect, useState} from 'react';
-import { collection, getDocs, query, where, orderBy, limit } from "firebase/firestore";
+import { collection, getDocs, query, where, orderBy, limit, startAfter } from "firebase/firestore";
 import { toast } from "react-toastify";
 import{ db} from "../firebase.config";
 import Sidebar from "../components/sidebar/Sidebar";
@@ -8,6 +8,7 @@ import ListingItem from "./listings/ListingItem";
 const Offer = () => {
     const [listings, setListings] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [lastFetched, setLastFetched] = useState(null)
     const [opened, setOpened] = useState(false);
 
     useEffect(() => {
@@ -23,6 +24,8 @@ const Offer = () => {
                 )
 
                 const querySnap = await getDocs(q);
+                const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+                setLastFetched(lastVisible)
                 const listings = [];
 
                 querySnap.forEach((doc) => {
@@ -41,6 +44,40 @@ const Offer = () => {
 
         fetchListings();
     }, [])
+
+    const loadMore = async () => {
+        try {
+            const listingsRef = collection(db, 'listings');
+
+            const q = query(
+                listingsRef,
+                where("offer", "==", true),
+                orderBy('timestemp', 'desc'),
+                startAfter(lastFetched),
+                limit(10)
+            )
+
+            const querySnap = await getDocs(q);
+
+            const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+            setLastFetched(lastVisible)
+
+            const listings = [];
+
+            querySnap.forEach((doc) => {
+                return listings.push({
+                    id: doc.id,
+                    data: doc.data()
+                })
+            })
+
+            setListings((prevState) => [...prevState, ...listings]);
+            setLoading(false)
+        } catch (err) {
+            toast.error('Could not fetch listings')
+        }
+    }
+
 
     return (
         <div className={`container-page ${opened && 'sidebar-open'}`}>
@@ -64,18 +101,29 @@ const Offer = () => {
                 <div>
                     {loading ? <h2 className={'heading-2'}>Loading...</h2> : listings &&
                     listings.length > 0 ? (
-                        <main>
-                            <ul className={'categoryListings'}>
-                                {listings.map((listing) => (
-                                    <ListingItem
-                                        listing={listing.data}
-                                        id={listing.id}
-                                        key={listing.id}
-                                        onDelete={() => console.log('deleted')}
-                                    />
-                                ))}
-                            </ul>
-                        </main>
+                        <>
+                            <main>
+                                <ul className={'categoryListings'}>
+                                    {listings.map((listing) => (
+                                        <ListingItem
+                                            listing={listing.data}
+                                            id={listing.id}
+                                            key={listing.id}
+                                            onDelete={() => console.log('deleted')}
+                                        />
+                                    ))}
+                                </ul>
+
+
+                            </main>
+
+                            <br/>
+                            <br/>
+
+                            {loadMore && listings.length >= 5 && (
+                                <p className={'loadMore'} onClick={loadMore}>Load More</p>
+                            )}
+                        </>
                     ) : (
                         <h2 className={'heading-2'}>No Offers found</h2>
                     )}
